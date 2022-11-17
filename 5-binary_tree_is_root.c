ee30fb2a85416b598b41ea8dfb9f7926c5ef88b7
abel2319 <abelbakary@gmail.com>
2022-11-17 12:44:06 +0100

    correction task5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -10,7 +10,6 @@
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node != NULL && node->parent == NULL)
-		if ((node->left != NULL) || (node->right != NULL))
-			return (1);
+		return (1);
 	return (0);
 }
