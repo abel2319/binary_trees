27745fb3b3edff9529bfc56a379f7d62bdabe61f
abel2319 <abelbakary@gmail.com>
2022-11-17 02:23:13 +0100

    madatory handle some corrections task2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,14 +2,14 @@
 
 
 /**
- * binary_tree_insert_rigth - inserts a node as the right-child of another node
+ * binary_tree_insert_right - inserts a node as the right-child of another node
  * @parent: pointer to the node to insert the left-child in
  * @value: value to store in the new node
  *
  * Return: a pointer to the created node
  * or NULL on failure or if parent is NULL
  */
-binary_tree_t *binary_tree_insert_rigth(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = NULL;
 
